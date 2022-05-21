809e0b016ca21d5bae408745cd4f35b43dcf2093
xliudaxia <xliudaxia@163.com>
2022-05-21 00:15:38 +0000

    feat: 搭建react开发环境

---
+++ index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Demo from './src/Demo'
+
+const container = document.getElementById('root');
+
+ReactDOM.render(<Demo />,container)
---
+++ src/Demo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Calendar} from 'antd';
+import 'antd/dist/antd.css';
+
+const Demo = ()=>{
+    return <>
+    <Calendar  />
+    </>
+}
+
+export default Demo;
