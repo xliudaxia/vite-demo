77893795c3c365c6d5f0f299b4d1e27e6c5590b4
xliudaxia <xliudaxia@163.com>
2022-06-09 03:12:56 +0000

    新增测试组件

---
+++ index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './src/Demo';
+import Test from './src/Upload'
+// import Table from './src/Table'
+import Demo from './src/Demo'
+import 'antd/dist/antd.css'
+
+const container = document.getElementById('root');
+ReactDOM.render(<Demo/>,container)
